32b7fe1d602e76ee2c0ca5a74bfa153cf655704b
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2020-12-02 23:31:38 +0300

    deploy

---
+++ precache-manifest.0da79d15e3e728498f567fa62810c9c0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1073a6aee254584c55",
+    "url": "/minin-vue-test/css/app.e9e8a83c.css"
+  },
+  {
+    "revision": "6a35704fe6df1710143c",
+    "url": "/minin-vue-test/css/chunk-4531856d.4a84d98c.css"
+  },
+  {
+    "revision": "4abf8197f5187b9f550f",
+    "url": "/minin-vue-test/css/chunk-ec9966fc.70b7fce5.css"
+  },
+  {
+    "revision": "80fcda1a1fde81deb989",
+    "url": "/minin-vue-test/css/chunk-vendors.24ce4a6c.css"
+  },
+  {
+    "revision": "e396c9b802393293aef42c5ecaf4f701",
+    "url": "/minin-vue-test/index.html"
+  },
+  {
+    "revision": "ee1073a6aee254584c55",
+    "url": "/minin-vue-test/js/app.3765268b.js"
+  },
+  {
+    "revision": "6a35704fe6df1710143c",
+    "url": "/minin-vue-test/js/chunk-4531856d.1fd2194c.js"
+  },
+  {
+    "revision": "4abf8197f5187b9f550f",
+    "url": "/minin-vue-test/js/chunk-ec9966fc.f6d88c4b.js"
+  },
+  {
+    "revision": "80fcda1a1fde81deb989",
+    "url": "/minin-vue-test/js/chunk-vendors.714316e0.js"
+  },
+  {
+    "revision": "0ae5fbcac0a2b7fc0529fa38b2431305",
+    "url": "/minin-vue-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/minin-vue-test/robots.txt"
+  }
+]);
